35109072ee7b1de96360895220cae7f3a3f7f3dd
Natalie <86774243+nnatalieh@users.noreply.github.com>
2024-01-06 00:26:28 -0600

    Add user's top tracks to user page

--- app/[user]/page.jsx
+++ app/[user]/page.jsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { getTopArtists } from "app/lib/spotify";
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
+
+import { getTopArtists, getTopTracks } from "app/lib/spotify";
 
 
 export default function Page({ params }) {
   const { data: session } = useSession();
   const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     if (session) {
       getTopArtists(session).then(data => setTopArtists(data.items));
+      getTopTracks(session).then(data => setTopTracks(data.items));
     }
   }, [session]);
 
@@ -24,6 +27,14 @@ export default function Page({ params }) {
           <h1 className="text-xl">Top Artists</h1>
           {(topArtists) && topArtists.map((artist, index) => (
             <p key={index}>{artist.name}</p>
+          ))}
+        </div>
+          
+        {/* User's top tracks */}
+        <div>
+          <h1 className="text-xl">Top Tracks</h1>
+          {(topTracks) && topTracks.map((track, index) => (
+            <p key={index}>{track.name}</p>
           ))}
         </div>
       </div>
--- app/lib/spotify.jsx
+++ app/lib/spotify.jsx
@@ -15,3 +15,20 @@ export const getTopArtists = async (session) => {
     console.error('Error:', error);
   }
 };
+
+export const getTopTracks = async (session) => {
+  try {
+    const response = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=5", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${session?.accessToken}` }
+    });
+
+    if (!response.ok) {
+      throw new Error(`getTopTracks error! status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
